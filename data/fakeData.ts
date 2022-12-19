423e3e720837e905172b00d4957bef3bd313cf8d
AlberttMoura <alberttmoura@gmail.com>
2022-12-18 21:18:20 -0300

    Ajustes

--- data/fakeData.ts
+++ data/fakeData.ts
@@ -13,16 +13,16 @@ export const fakePontosTuristicos: PontoTuristico[] = [
 		thumbnail:
 			'https://www.didigalvao.com.br/wp-content/uploads/2021/03/DAD76411-F7C2-4C81-AC7C-F3F9B8D6C126.jpeg',
 	},
-	{
-		id: 2,
-		nome: 'Morro da Conceição',
-		descricao: `Várias pessoas se dirigem ao Morro da Conceição, que até pouco tempo atrás era pouco conhecido, hoje é destino certo de quem curte uma caminhada tranquila em meio a uma estrada aberta ao lado da mata da caatinga ou um passeio de bike, como um grande prêmio ao final, uma vista panorâmica da cidade.`,
-		galeria: [],
-		coords: {
-			longitude: -39.31,
-			latitude: -8.51,
-		},
-		thumbnail:
-			'https://www.didigalvao.com.br/wp-content/uploads/2021/03/DAD76411-F7C2-4C81-AC7C-F3F9B8D6C126.jpeg',
-	},
+	// {
+	// 	id: 2,
+	// 	nome: 'Morro da Conceição',
+	// 	descricao: `Várias pessoas se dirigem ao Morro da Conceição, que até pouco tempo atrás era pouco conhecido, hoje é destino certo de quem curte uma caminhada tranquila em meio a uma estrada aberta ao lado da mata da caatinga ou um passeio de bike, como um grande prêmio ao final, uma vista panorâmica da cidade.`,
+	// 	galeria: [],
+	// 	coords: {
+	// 		longitude: -39.31,
+	// 		latitude: -8.51,
+	// 	},
+	// 	thumbnail:
+	// 		'https://www.didigalvao.com.br/wp-content/uploads/2021/03/DAD76411-F7C2-4C81-AC7C-F3F9B8D6C126.jpeg',
+	// },
 ]
